fb89d231ca3d5fd7c35e842369c281f1976b5625
Kalpani-Bandara <kalpaniprarthana29@gmail.com>
2026-03-05 15:59:16 +0530

    Set up Azure SQL database

--- SmartPostOffice/Program.cs
+++ SmartPostOffice/Program.cs
@@ -1,29 +1,30 @@
+// Program.cs
+//using Microsoft.EntityFrameworkCore;
+//using SmartPostOffice.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// 1. Add MVC (Controllers + Views)
 builder.Services.AddControllersWithViews();
+
+// 2. Add Database Context
+//    This INJECTS our PostOfficeDbContext into any class that needs it
+//    The connection string comes from appsettings.json
+//builder.Services.AddDbContext<PostOfficeDbContext>(options =>
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();       
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapStaticAssets();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-
-app.Run();
