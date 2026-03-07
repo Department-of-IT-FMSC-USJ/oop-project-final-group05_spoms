de85c8714a469df078f69f19cf96395ffdc0cbe6
Kalpani-Bandara <kalpaniprarthana29@gmail.com>
2026-03-07 11:24:49 +0530

    S1_Task_01 Set Up the System Environment

---
+++ SmartPostOffice/Migrations/20260307054922_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartPostOffice.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ServiceRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReferenceNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ServiceType = table.Column<int>(type: "int", nullable: false),
+                    SenderName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SenderPhone = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    SenderAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReceiverPhone = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    ReceiverAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EstimatedWeightGrams = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Destination = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ItemDescription = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    SellerProfitAmount = table.Column<decimal>(type: "decimal(10,2)", nullable: true),
+                    TotalCODAmount = table.Column<decimal>(type: "decimal(10,2)", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceRequests", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServiceRequests");
+        }
+    }
+}
