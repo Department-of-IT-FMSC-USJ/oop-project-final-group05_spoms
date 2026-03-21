90fe41ea1b1898e6e786a3ab621a973c7f63ead2
Kalpani-Bandara <kalpaniprarthana29@gmail.com>
2026-03-22 00:00:24 +0530

    S4_Task_04 Fix: resolve Telimail routing and form submission issues

--- SmartPostOffice/Controllers/TelimailController.cs
+++ SmartPostOffice/Controllers/TelimailController.cs
@@ -9,45 +9,48 @@ namespace SmartPostOffice.Controllers
     public class TelimailController : Controller
     {
         private readonly PostOfficeDbContext _db;
-        private readonly IPaymentService     _payment;
+        private readonly IPaymentService _payment;
 
         public TelimailController(PostOfficeDbContext db, IPaymentService payment)
         { _db = db; _payment = payment; }
 
-       
+
         public IActionResult Index() => View();
 
-       
+
         public IActionResult Send() => View(new TelimailMessage());
 
-        
+
         [HttpPost, ValidateAntiForgeryToken]
         public IActionResult Send(TelimailMessage model)
         {
+            ModelState.Remove(nameof(TelimailMessage.TelimailReference));
+            ModelState.Remove(nameof(TelimailMessage.PaymentStatus));
+
             if (!ModelState.IsValid) return View(model);
 
-            model.TelimailReference = $"TLM-{DateTime.Now:yyyyMMdd}-{new Random().Next(1000,9999)}";
-            model.PaymentStatus     = "Pending";
+            model.TelimailReference = $"TLM-{DateTime.Now:yyyyMMdd}-{new Random().Next(1000, 9999)}";
+            model.PaymentStatus = "Pending";
             _db.TelimailMessages.Add(model);
             _db.SaveChanges();
 
             return RedirectToAction("Pay", new { id = model.Id });
         }
 
-     
+
         public IActionResult Pay(int id)
         {
             var msg = _db.TelimailMessages.Find(id);
             if (msg == null) return NotFound();
-            ViewBag.Amount       = msg.CalculateTotal();
-            ViewBag.Reference    = msg.TelimailReference;
+            ViewBag.Amount = msg.CalculateTotal();
+            ViewBag.Reference = msg.TelimailReference;
             ViewBag.ServiceLabel = msg.GetServiceLabel();
-            ViewBag.BookingId    = id;
-            ViewBag.Module       = "Telemail";
+            ViewBag.BookingId = id;
+            ViewBag.Module = "Telimail";
             return View("~/Views/Payment/Pay.cshtml");
         }
 
-     
+
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Pay(int bookingId, string cardNumber,
                                               string expiryDate, string cvv,
@@ -65,18 +68,18 @@ namespace SmartPostOffice.Controllers
                 return RedirectToAction("Pay", new { id = bookingId });
             }
 
-            msg.PaymentStatus    = "Paid";
+            msg.PaymentStatus = "Paid";
             msg.PaymentReference = result.Reference;
-            msg.PaidAt           = DateTime.Now;
+            msg.PaidAt = DateTime.Now;
             _db.SaveChanges();
 
             _db.CashBookEntries.Add(new TelimailEntry
             {
                 TransactionId = null,
-                Amount        = msg.CalculateTotal(),
+                Amount = msg.CalculateTotal(),
                 PaymentMethod = "Online",
-                EntryType     = "CREDIT",
-                EntryDate     = DateTime.Now
+                EntryType = "CREDIT",
+                EntryDate = DateTime.Now
             });
             _db.SaveChanges();
 
@@ -84,7 +87,7 @@ namespace SmartPostOffice.Controllers
             return RedirectToAction("Confirmation", new { id = msg.Id });
         }
 
-       
+
         public IActionResult Confirmation(int id)
         {
             var msg = _db.TelimailMessages.Find(id);
@@ -92,7 +95,7 @@ namespace SmartPostOffice.Controllers
             return View(msg);
         }
 
-        
+
         [Authorize(AuthenticationSchemes = "OfficerCookies")]
         public IActionResult Inbox()
         {
